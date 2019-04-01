78cdf3e103d3ee0c5a48cd0c621f753058415ec0
Anu bhattarai <anu.bhatrai1998@gmail.com>
2019-04-01 12:04:09 +0545

    added switch case

--- app/src/main/java/com/e/imageview/MainActivity.java
+++ app/src/main/java/com/e/imageview/MainActivity.java
@@ -2,12 +2,44 @@ package com.e.imageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private RadioButton rbRajesh, rbDaya, rbBhuwan;
+    private ImageView imgHero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rbRajesh = findViewById(R.id.rbRajesh);
+        rbDaya = findViewById(R.id.rbDaya);
+        rbBhuwan = findViewById(R.id.rbBhuwan);
+        imgHero = findViewById(R.id.imgHero);
+
+        rbRajesh.setOnClickListener(this);
+        rbDaya.setOnClickListener(this);
+        rbBhuwan.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rbRajesh:
+                break;
+
+            case R.id.rbDaya :
+                break;
+
+            case R.id.rbBhuwan:
+                break;
+
+        }
     }
 }
